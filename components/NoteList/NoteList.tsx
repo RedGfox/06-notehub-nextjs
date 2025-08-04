ccbf7b209a51f9665bf64462ae12e024c33e3e36
RedGfox <zim238kosmit@gmail.com>
2025-08-04 12:58:33 +0300

    fix components

--- components/NoteList/NoteList.tsx
+++ components/NoteList/NoteList.tsx
@@ -1,4 +1,5 @@
 import css from './NoteList.module.css';
+import Link from 'next/link';
 
 import toast from 'react-hot-toast';
 import { Note } from '@/types/note';
@@ -27,7 +28,9 @@ export default function NoteList({ notes }: NoteListProps) {
     <ul className={css.list}>
       {notes.map(note => (
         <li className={css.listItem} key={note.id}>
-          <h2 className={css.title}>{note.title}</h2>
+          <Link href={`/notes/${note.id}`} className={css.link}>
+            <h2 className={css.title}>{note.title}</h2>
+          </Link>
           <p className={css.content}>{note.content}</p>
           <div className={css.footer}>
             <span className={css.tag}>{note.tag}</span>
